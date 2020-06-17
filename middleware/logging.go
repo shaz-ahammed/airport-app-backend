6f58afbcc89c605d9a6d88713d2d5bfcd3b00e8e
Janusz Piechówka <januszpiechowka@gmail.com>
2020-06-17 20:01:15 +0200

    Add logging.go file with middleware to enable request logging

---
+++ middleware/logging.go
@@ -0,0 +1,57 @@
+package middleware
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
+	"net/http"
+	"os"
+	"time"
+)
+
+var consoleLogger = zerolog.New(os.Stderr).Output(zerolog.ConsoleWriter{
+	Out:        os.Stderr,
+	NoColor:    false,
+	TimeFormat: time.RFC1123Z,
+})
+
+func ZerologConsoleRequestLogging() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		startTime := time.Now()
+
+		var requestPath string
+
+		if requestRawQuery := context.Request.URL.RawQuery; requestRawQuery == "" {
+			requestPath = context.Request.URL.Path
+		} else {
+			requestPath = fmt.Sprintf("%s?%s", context.Request.URL.Path, requestRawQuery)
+		}
+
+		context.Next()
+
+		latency := time.Since(startTime)
+
+		subLogger := consoleLogger.With().Timestamp().
+			Int("http-status", context.Writer.Status()).
+			Str("method", context.Request.Method).
+			Str("request-path", requestPath).
+			Str("client-ip", context.ClientIP()).
+			Str("user-agent", context.Request.UserAgent()).
+			Int64("content-length", context.Request.ContentLength).
+			Dur("latency", latency).
+			Logger()
+
+		logMsg := "Request"
+		if len(context.Errors) > 0 {
+			logMsg = context.Errors.String()
+		}
+
+		if context.Writer.Status() >= http.StatusInternalServerError {
+			subLogger.Error().Msg(logMsg)
+		} else if context.Writer.Status() >= http.StatusBadRequest && context.Writer.Status() < http.StatusInternalServerError {
+			subLogger.Warn().Msg(logMsg)
+		} else {
+			subLogger.Info().Msg(logMsg)
+		}
+	}
+}
