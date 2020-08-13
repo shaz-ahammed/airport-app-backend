a8005e15ad32727df3bbcbc827d990a61fd32f2b
Janusz Piechówka <januszpiechowka@gmail.com>
2020-08-13 15:54:12 +0200

    Refactor code. Add new packages. Add new handlers. Add config. Add generation of self-signed certificates

---
+++ server/routes.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+	"golang-app-starter/config"
+	"golang-app-starter/middleware"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+)
+
+// Main API routes
+func (srv *AppServer) setupRoutesAndMiddleware() {
+	srv.router.GET("/health/", srv.handleHealth)
+	srv.router.GET("/ping", srv.handlePing)
+	srv.router.GET("/", srv.handleIndex)
+
+	// Middleware
+	log.Info().Msg("Configuring GIN middleware")
+	srv.router.Use(gin.Recovery()) // Default recovery middleware
+
+	srv.router.Use(middleware.DisableCache())
+	srv.router.Use(middleware.AddSecurityHeaders(config.EnableTls))
+	srv.router.Use(middleware.HandleFaviconRequests())
+
+	if config.EnableDetailedRequestLogging {
+		log.Info().Msg("Enabling request logging middleware")
+		srv.router.Use(middleware.ZerologConsoleRequestLogging())
+	}
+}
