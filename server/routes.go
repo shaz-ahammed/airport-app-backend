0ff7744f63f2a97ddb87cb1cc7b0160949ec31d2
Sathya Rupa.S <sathyarupa2002@gmail.com>
2024-04-02 11:59:14 +0530

    Resolved merge conflicts

--- server/routes.go
+++ server/routes.go
@@ -3,40 +3,15 @@ package server
 import (
 	"airport-app-backend/config"
 	"airport-app-backend/middleware"
-	"airport-app-backend/services"
 
 	"airport-app-backend/controllers"
-
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 )
 
 // Main API routes
 func (srv *AppServer) setupRoutesAndMiddleware() {
-
-	log.Info().Msg("Connecting to postgres database")
-
-	DB, err := ConnectToDB()
-	if err != nil {
-		log.Info().Err(err).Msg("Database connection failed")
-		return
-	}
-
-	err = MigrateAll(DB)
-	if err != nil {
-		log.Info().Err(err).Msg("Database migration failed")
-		return
-	}
-	log.Info().Msg("Database migration Successful")
-
-
-
-
-	serviceRepo := services.NewServiceRepository(DB)
-	controllerRepo := controllers.NewControllerRepository(serviceRepo)
-
-
-	srv.router.GET("/health/", controllerRepo.HandleHealth)
+	srv.router.GET("/health/", controllers.HandleHealth)
 
 	// Middleware
 	log.Info().Msg("Configuring GIN middleware")
