4c3e0ed5091b46070d63b7f1ed85e0e30655b3e1
Shaz Ahammed <shazahammed2000@gmail.com>
2024-04-02 11:03:28 +0530

    #63 [Shaz|Sathya] Mocking ServiceRepo

--- services/service_repository.go
+++ services/service_repository.go
@@ -2,13 +2,16 @@ package services
 
 import "gorm.io/gorm"
 
-
 type ServiceRepository struct {
 	db *gorm.DB
 }
 
+type IServiceRepository interface {
+	NewServiceRepository(db *gorm.DB) ServiceRepository
+}
+
 func NewServiceRepository(db *gorm.DB) ServiceRepository {
 	return ServiceRepository{
-		db : db,
+		db: db,
 	}
 }
