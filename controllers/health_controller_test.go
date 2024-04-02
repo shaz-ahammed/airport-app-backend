8dab927beae43158aba89cd48887a110796c014e
Shaz Ahammed <shazahammed2000@gmail.com>
2024-04-02 15:24:58 +0530

    #63 [Shaz] Refactor test

--- controllers/health_controller_test.go
+++ controllers/health_controller_test.go
@@ -12,42 +12,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHandleHealthControllerSample(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-
-	mockService := mocks.NewMockIHealthRepository(mockCtrl)
-	controllerRepo := NewControllerRepository(mockService)
-  response := models.AppHealth {
-    Goroutines: 5,
-  }
-	mockService.EXPECT().GetAppHealth().Return(response)
-	recorder := httptest.NewRecorder()
-	ctx, _ := gin.CreateTestContext(recorder)
-
-	ctx.Request, _ = http.NewRequest("GET", "/health", nil)
-
-	controllerRepo.HandleHealth(ctx)
-	assert.Equal(t, http.StatusOK, ctx.Writer.Status())
-
-}
-
 func TestHandleHealthController(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
-
 	mockService := mocks.NewMockIHealthRepository(mockCtrl)
 	controllerRepo := NewControllerRepository(mockService)
-
 	appHealthMock := models.AppHealth{Goroutines: 5}
-
 	mockService.EXPECT().GetAppHealth().Return(appHealthMock)
 	recorder := httptest.NewRecorder()
 	ctx, _ := gin.CreateTestContext(recorder)
 
 	ctx.Request, _ = http.NewRequest("GET", "/health", nil)
-
 	controllerRepo.HandleHealth(ctx)
-	assert.Equal(t, http.StatusOK, ctx.Writer.Status())
 
+	assert.Equal(t, http.StatusOK, ctx.Writer.Status())
 }
