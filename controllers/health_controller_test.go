205e174ea2f97ff8456cbe3ca741d85634718400
Shaz Ahammed <shazahammed2000@gmail.com>
2024-04-02 11:03:28 +0530

    #63 [Shaz|Sathya] Mocking ServiceRepo

--- controllers/health_controller_test.go
+++ controllers/health_controller_test.go
@@ -1,21 +1,27 @@
 package controllers
 
 import (
-"net/http"
-"net/http/httptest"
-"testing"
-"github.com/gin-gonic/gin"
-"github.com/stretchr/testify/assert"
+	mocker "airport-app-backend/mocks/mocks"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHandleHealthController(t *testing.T) {
-  router := gin.Default()
-  router.GET("/health", HandleHealth)
-  req, err := http.NewRequest("GET", "/health", nil)
-  if err != nil {
-    t.Fatal(err)
-  }
-  rr := httptest.NewRecorder()
-  router.ServeHTTP(rr, req)
-  assert.Equal(t,http.StatusOK,rr.Code)
+	mockCtrl := gomock.NewController(t)
+	mockService := mocker.NewServiceRepository(mockCtrl)
+	controllerRepo := NewControllerRepository(mockService)
+	router := gin.Default()
+	router.GET("/health", controllerRepo.HandleHealth)
+	req, err := http.NewRequest("GET", "/health", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusOK, rr.Code)
 }
