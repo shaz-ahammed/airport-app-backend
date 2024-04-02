bce2b315f990071ee18dcd6d9fdb4bb654cf7d93
Shaz Ahammed <shazahammed2000@gmail.com>
2024-04-02 13:54:40 +0530

    #63 [Shaz|Sathya] removed /server from gitignore

---
+++ server/postgres.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"airport-app-backend/models"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog/log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+
+type Config struct {
+	host     string
+	user     string
+	password string
+	dbname   string
+	port     string
+	sslmode  string
+}
+
+func ConnectToDB() (*gorm.DB,error) {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+		log.Info().Err(err).Msg("Database connection failed")
+		return nil, err
+	}
+	config := Config{
+		host:     os.Getenv("HOST"),
+		user:     os.Getenv("POSTGRES_USER"),
+		password: os.Getenv("POSTGRES_PASSWORD"),
+		dbname:   os.Getenv("DB_NAME"),
+		port:     os.Getenv("PORT"),
+		sslmode:  os.Getenv("SSL_MODE"),
+	}
+
+	dsn := fmt.Sprintf(
+		`host=%s port=%s user=%s password=%s dbname=%s  sslmode=%s`,
+		config.host, config.port, config.user, config.password, config.dbname, config.sslmode,
+	)
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Info().Err(err).Msg("Database connection failed")
+		return nil, err
+	}
+	log.Info().Msg("Database connection successful")
+
+	return DB, err
+
+}
+
+func MigrateAll(db *gorm.DB) error {
+	err := models.AircraftsTable(db)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
