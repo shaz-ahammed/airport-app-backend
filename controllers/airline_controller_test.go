54ea4ba6b8c18dfed03833c68e3fea7900265bd5
jaicharan <jaicharan.santhanam@thoughtworks.com>
2024-04-20 10:44:12 +0530

    #14 [Sathya|Jai] Controller test for Creating a new airline

--- controllers/airline_controller_test.go
+++ controllers/airline_controller_test.go
@@ -3,8 +3,11 @@ package controllers
 import (
 	"airport-app-backend/mocks"
 	"airport-app-backend/models"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -39,6 +42,34 @@ func TestHandleAirlineByIdController(t *testing.T) {
 	mockService.EXPECT().GetAirlineById(gomock.Any(), ctx, gomock.Any()).Return(&mockAirlines, nil)
 	ctx.Request, _ = http.NewRequest("GET", "airline/12332", nil)
 	controllerRepo.HandleGetAirlineById(ctx)
+	fmt.Println(ctx.Writer.Status())
 	assert.Equal(t, http.StatusOK, ctx.Writer.Status())
+
+}
+
+func TestHandleCreateNewAirline(t *testing.T) {
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockService := mocks.NewMockIAirlineRepository(mockCtrl)
+	controllerRepo := NewAirlineControllerRepository(mockService)
+	recorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(recorder)
+	airline := models.Airlines{
+		Name: "XYZAirlines",
+	}
+	mockService.EXPECT().CreateNewAirline(gomock.Any(), ctx, &airline).Return(nil)
+	reqBody := `{"name":"XYZAirlines"}`
+
+	ctx.Request, _ = http.NewRequest("POST", "/airline", strings.NewReader(reqBody))
+	controllerRepo.HandleCreateNewAirline(ctx)
+	fmt.Println(ctx.Writer.Status())
+	assert.Equal(t, http.StatusCreated, ctx.Writer.Status())
+
+	var response models.Airlines
+	err := json.Unmarshal([]byte(reqBody), &response)
+	assert.NoError(t, err)
+
+	assert.Equal(t, airline.Name, response.Name)
 
 }
