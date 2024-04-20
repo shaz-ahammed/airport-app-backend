940096c25a32800077f3970962ffaaec0bfb67f9
jaicharan <jaicharan.santhanam@thoughtworks.com>
2024-04-20 10:43:20 +0530

    #14 [Sathya|Jai] Created a endpoint to Create a new airline

--- server/airline_router.go
+++ server/airline_router.go
@@ -10,7 +10,10 @@ import (
 func (srv *AppServer) AirlineRouter(db *gorm.DB) {
 	airlineService := services.NewServiceRepository(db)
 	airlineController := controllers.NewAirlineControllerRepository(airlineService)
+<<<<<<<
+=======
 
+>>>>>>>
 	srv.router.GET("/airline/", airlineController.HandleGetAirline)
 	srv.router.GET("/airline/:id", airlineController.HandleGetAirlineById)
 	srv.router.POST("/airline", airlineController.HandleCreateNewAirline)
