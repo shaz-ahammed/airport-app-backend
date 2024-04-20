f0e2982dcc377d607a80fa1026f5175fd9674fcc
jaicharan <jaicharan.santhanam@thoughtworks.com>
2024-04-20 10:44:12 +0530

    #14 [Sathya|Jai] Controller test for Creating a new airline

--- controllers/airline_controller_test.go
+++ controllers/airline_controller_test.go
@@ -42,6 +42,7 @@ func TestHandleAirlineByIdController(t *testing.T) {
 	mockService.EXPECT().GetAirlineById(gomock.Any()).Return(&mockAirlines, nil)
 	ctx.Request, _ = http.NewRequest("GET", "airline/12332", nil)
 	controllerRepo.HandleGetAirlineById(ctx)
+	fmt.Println(ctx.Writer.Status())
 	assert.Equal(t, http.StatusOK, ctx.Writer.Status())
 }
 
