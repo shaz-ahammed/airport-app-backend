edb0d0aa0b66aa9477455060ecefd7bf36913769
Shaz Ahammed <153790994+shaz-ahammed@users.noreply.github.com>
2024-04-22 15:52:01 +0530

    [Shaz] removed sample table (#28)
    * removed sample table
    * install go test report with make install
    * install mockgen with make install 
    * removed sonar command from bootrun

--- database/db_migration/V004__PopulateWithRandValues.sql
+++ database/db_migration/V004__PopulateWithRandValues.sql
@@ -2,7 +2,7 @@ INSERT INTO gates (gate_number, floor_number)
 SELECT
     floor(random() * 100) + 1,
     floor(random() * 10) + 1
-FROM generate_series(1, 100)
+FROM generate_series(1, 23)
 ON CONFLICT (gate_number) DO NOTHING;
 
 INSERT INTO airlines (name)
