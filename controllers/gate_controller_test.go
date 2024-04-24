a9c816ec9f3561a7605c846e4b14634fe3acd36b
madhavan <madhavankca@gmail.com>
2024-04-24 11:10:34 +0530

    #15[Madhavan|Jai]included beforeEach method for tests

--- controllers/gate_controller_test.go
+++ controllers/gate_controller_test.go
@@ -14,8 +14,10 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
+
 var GET_GATE_BY_ID = "/gate/123"
 var GET_ALL_GATES = "gates"
+var CREATE_NEW_GATE = "/gate"
 
 var gateMockService *mocks.MockIGateRepository
 var gateMockController *GateControllerRepository
@@ -85,106 +87,76 @@ func TestHandleGetGateByIdWhenServiceReturnsError(t *testing.T) {
 }
 
 func TestHandleCreateNewGate(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	mockService := mocks.NewMockIGateRepository(mockCtrl)
-	controllerRepo := NewGateRepository(mockService)
-	recorder := httptest.NewRecorder()
-	ctx, _ := gin.CreateTestContext(recorder)
+	beforeEachGateTest(t)
 	gate := models.Gate{
 		GateNumber:  1,
 		FloorNumber: 1,
 	}
-	mockService.EXPECT().CreateNewGate(&gate).Return(nil)
+	gateMockService.EXPECT().CreateNewGate(&gate).Return(nil)
 	reqBody := `{"gate_number" : 1,"floor_number" : 1}`
 	var gateModel models.Gate
 
-	ctx.Request, _ = http.NewRequest("POST", "/gate", strings.NewReader(reqBody))
-	controllerRepo.HandleCreateNewGate(ctx)
+	gateContext.Request, _ = http.NewRequest("POST", "CREATE_NEW_GATE", strings.NewReader(reqBody))
+	gateMockController.HandleCreateNewGate(gateContext)
 	err := json.Unmarshal([]byte(reqBody), &gateModel)
 
-	assert.Equal(t, http.StatusOK, ctx.Writer.Status())
+	assert.Equal(t, http.StatusOK, gateContext.Writer.Status())
 	assert.NoError(t, err)
 	assert.Equal(t, gate.GateNumber, gateModel.GateNumber)
 	assert.Equal(t, gate.FloorNumber, gateModel.FloorNumber)
 }
 
 func TestHandleCreateNewGateWhenTheMandatoryValueIsAbsent(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	mockService := mocks.NewMockIGateRepository(mockCtrl)
-	controllerRepo := NewGateRepository(mockService)
-	recorder := httptest.NewRecorder()
-	ctx, _ := gin.CreateTestContext(recorder)
+	beforeEachGateTest(t)
 	reqBody := `{"gate_number":,"floor_number":2}`
 
-	ctx.Request, _ = http.NewRequest("POST", "/gate", strings.NewReader(reqBody))
-	controllerRepo.HandleCreateNewGate(ctx)
+	gateContext.Request, _ = http.NewRequest("POST", "CREATE_NEW_GATE", strings.NewReader(reqBody))
+	gateMockController.HandleCreateNewGate(gateContext)
 
-	assert.Equal(t, http.StatusBadRequest, ctx.Writer.Status())
+	assert.Equal(t, http.StatusBadRequest, gateContext.Writer.Status())
 }
 
 func TestHandleCreateNewGateWhenTheRequestPayloadIsEmpty(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	mockService := mocks.NewMockIGateRepository(mockCtrl)
-	controllerRepo := NewGateRepository(mockService)
-	recorder := httptest.NewRecorder()
-	ctx, _ := gin.CreateTestContext(recorder)
+	beforeEachGateTest(t)
 	reqBody := `{}`
 
-	ctx.Request, _ = http.NewRequest("POST", "/gate", strings.NewReader(reqBody))
-	controllerRepo.HandleCreateNewGate(ctx)
+	gateContext.Request, _ = http.NewRequest("POST", "CREATE_NEW_GATE", strings.NewReader(reqBody))
+	gateMockController.HandleCreateNewGate(gateContext)
 
-	assert.Equal(t, http.StatusBadRequest, ctx.Writer.Status())
+	assert.Equal(t, http.StatusBadRequest, gateContext.Writer.Status())
 }
 
 func TestHandleCreateNewGateWhenTheMandatoryKeyIsAbsent(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	mockService := mocks.NewMockIGateRepository(mockCtrl)
-	controllerRepo := NewGateRepository(mockService)
-	recorder := httptest.NewRecorder()
-	ctx, _ := gin.CreateTestContext(recorder)
+	beforeEachGateTest(t)
 	reqBody := `{"gate_number":2}`
 
-	ctx.Request, _ = http.NewRequest("POST", "/gate", strings.NewReader(reqBody))
-	controllerRepo.HandleCreateNewGate(ctx)
+	gateContext.Request, _ = http.NewRequest("POST", "CREATE_NEW_GATE", strings.NewReader(reqBody))
+	gateMockController.HandleCreateNewGate(gateContext)
 
-	assert.Equal(t, http.StatusBadRequest, ctx.Writer.Status())
+	assert.Equal(t, http.StatusBadRequest, gateContext.Writer.Status())
 }
 
 func TestHandleCreateNewGateWhenDataOfDifferentDatatypeIsGiven(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	mockService := mocks.NewMockIGateRepository(mockCtrl)
-	controllerRepo := NewGateRepository(mockService)
-	recorder := httptest.NewRecorder()
-	ctx, _ := gin.CreateTestContext(recorder)
+	beforeEachGateTest(t)
 	reqBody := `{"gate_number":"one","floor_number":20}`
 
-	ctx.Request, _ = http.NewRequest("POST", "/gate", strings.NewReader(reqBody))
-	controllerRepo.HandleCreateNewGate(ctx)
+	gateContext.Request, _ = http.NewRequest("POST", "CREATE_NEW_GATE", strings.NewReader(reqBody))
+	gateMockController.HandleCreateNewGate(gateContext)
 
-	assert.Equal(t, http.StatusBadRequest, ctx.Writer.Status())
+	assert.Equal(t, http.StatusBadRequest, gateContext.Writer.Status())
 }
 
 func TestHandleCreateNewGateWhereErrorIsThrownInServiceLayer(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	mockService := mocks.NewMockIGateRepository(mockCtrl)
-	controllerRepo := NewGateRepository(mockService)
-	recorder := httptest.NewRecorder()
-	ctx, _ := gin.CreateTestContext(recorder)
+	beforeEachGateTest(t)
 	gate := models.Gate{
 		GateNumber:  3,
 		FloorNumber: 6,
 	}
 	reqBody := `{"gate_number":3, "floor_number":6}`
 
-	mockService.EXPECT().CreateNewGate(&gate).Return(errors.New("invalid Request"))
-	ctx.Request, _ = http.NewRequest("POST", "/gate", strings.NewReader(reqBody))
-	controllerRepo.HandleCreateNewGate(ctx)
+	gateMockService.EXPECT().CreateNewGate(&gate).Return(errors.New("invalid Request"))
+	gateContext.Request, _ = http.NewRequest("POST", "CREATE_NEW_GATE", strings.NewReader(reqBody))
+	gateMockController.HandleCreateNewGate(gateContext)
 
-	assert.Equal(t, http.StatusBadRequest, ctx.Writer.Status())
+	assert.Equal(t, http.StatusBadRequest, gateContext.Writer.Status())
 }
