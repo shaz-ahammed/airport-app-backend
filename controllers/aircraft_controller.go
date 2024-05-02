2a4ae6437ad5450e9196a793ed01294c4d0d3b13
Sathya Rupa.S <sathyarupa2002@gmail.com>
2024-05-02 11:01:36 +0530

    Refactor aircraft_controller

--- controllers/aircraft_controller.go
+++ controllers/aircraft_controller.go
@@ -1,28 +1,28 @@
 package controllers
 
 import (
-	"airport-app-backend/models"
-	"airport-app-backend/repositories"
-	"github.com/gin-gonic/gin/binding"
-	"net/http"
-	"strconv"
+  "airport-app-backend/models"
+  "airport-app-backend/repositories"
+  "github.com/gin-gonic/gin/binding"
+  "net/http"
+  "strconv"
 
-	"github.com/gin-gonic/gin"
+  "github.com/gin-gonic/gin"
 )
 
 var (
-	START_YEAR = 1970
-	NO_FILTER  = -1
+  START_YEAR = 1970
+  NO_FILTER  = -1
 )
 
 type AircraftController struct {
-	repository repositories.IAircraftRepository
+  repository repositories.IAircraftRepository
 }
 
 func NewAircraftController(repository repositories.IAircraftRepository) *AircraftController {
-	return &AircraftController{
-		repository: repository,
-	}
+  return &AircraftController{
+    repository: repository,
+  }
 }
 
 // @Summary Get all aircrafts
@@ -37,27 +37,27 @@ func NewAircraftController(repository repositories.IAircraftRepository) *Aircraf
 // @Success 200  "ok"
 // @Failure 500 "Internal server error"
 func (ac *AircraftController) HandleGetAllAircrafts(ctx *gin.Context) {
-	// TODO: Convert to using a pagination library to handle this and other edge cases
-	page, _ := strconv.Atoi(ctx.Query("page"))
-	if page < 0 {
-		ctx.JSON(400, gin.H{"msg": "Page number must be greater than 0"})
-		return
-	}
-	year, _ := strconv.Atoi(ctx.Query("year"))
-	if year < START_YEAR {
-		year = NO_FILTER
-	}
-	capacity, _ := strconv.Atoi(ctx.Query("capacity"))
-	if capacity < 0 {
-		capacity = NO_FILTER
-	}
+  // TODO: Convert to using a pagination library to handle this and other edge cases
+  page, _ := strconv.Atoi(ctx.Query("page"))
+  if page < 0 {
+    ctx.JSON(400, gin.H{"msg": "Page number must be greater than 0"})
+    return
+  }
+  year, _ := strconv.Atoi(ctx.Query("year"))
+  if year < START_YEAR {
+    year = NO_FILTER
+  }
+  capacity, _ := strconv.Atoi(ctx.Query("capacity"))
+  if capacity < 0 {
+    capacity = NO_FILTER
+  }
 
-	aircrafts, err := ac.repository.RetrieveAllAircrafts(page, capacity, year)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"Error": "Internal server error"})
-		return
-	}
-	ctx.JSON(http.StatusOK, aircrafts)
+  aircrafts, err := ac.repository.RetrieveAllAircrafts(page, capacity, year)
+  if err != nil {
+    ctx.JSON(http.StatusInternalServerError, gin.H{"Error": "Internal server error"})
+    return
+  }
+  ctx.JSON(http.StatusOK, aircrafts)
 }
 
 // @Summary			Get aircraft by Id
@@ -70,13 +70,13 @@ func (ac *AircraftController) HandleGetAllAircrafts(ctx *gin.Context) {
 // @Success 		200		"ok"
 // @Failure 		400		"Aircraft not found"
 func (ac *AircraftController) HandleGetAircraft(ctx *gin.Context) {
-	aircraftId := ctx.Param("id")
-	aircraft, err := ac.repository.RetrieveAircraft(aircraftId)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"Error": "Incorrect aircraft id: " + aircraftId})
-		return
-	}
-	ctx.JSON(http.StatusOK, aircraft)
+  aircraftId := ctx.Param("id")
+  aircraft, err := ac.repository.RetrieveAircraft(aircraftId)
+  if err != nil {
+    ctx.JSON(http.StatusBadRequest, gin.H{"Error": "Incorrect aircraft id: " + aircraftId})
+    return
+  }
+  ctx.JSON(http.StatusOK, aircraft)
 }
 
 // @Summary Create aircraft
@@ -90,21 +90,21 @@ func (ac *AircraftController) HandleGetAircraft(ctx *gin.Context) {
 // @Success 201  "Aircraft created"
 // @Failure 500 "Internal server error"
 func (ac *AircraftController) HandleCreateNewAircraft(ctx *gin.Context) {
-	var aircraft models.Aircraft
+  var aircraft models.Aircraft
 
-	airlineId := ctx.Param("airline_id")
-	err := ctx.ShouldBindWith(&aircraft, binding.JSON)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
+  airlineId := ctx.Param("airline_id")
+  err := ctx.ShouldBindWith(&aircraft, binding.JSON)
+  if err != nil {
+    ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+    return
+  }
 
-	err = ac.repository.InsertAircraft(aircraft, airlineId)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	ctx.JSON(http.StatusCreated, "Aircraft created Successfully")
+  err = ac.repository.InsertAircraft(aircraft, airlineId)
+  if err != nil {
+    ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+    return
+  }
+  ctx.JSON(http.StatusCreated, "Aircraft created Successfully")
 }
 
 // @Router /airlines/{airline_id}/aircraft/{id} [put]
@@ -119,21 +119,21 @@ func (ac *AircraftController) HandleCreateNewAircraft(ctx *gin.Context) {
 // @Success 200  "ok"
 // @Failure 400 "Bad request"
 func (ac *AircraftController) HandleUpdateAircraft(ctx *gin.Context) {
-	var aircraft models.Aircraft
-	airlineId := ctx.Param(`airline_id`)
-	aircraftId := ctx.Param(`id`)
+  var aircraft models.Aircraft
+  airlineId := ctx.Param(`airline_id`)
+  aircraftId := ctx.Param(`id`)
 
-	err := ctx.ShouldBindWith(&aircraft, binding.JSON)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
-		return
-	}
-	repositoryError := ac.repository.UpdateAircraft(&aircraft, aircraftId, airlineId)
-	if repositoryError != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"Error": repositoryError.Error()})
-		return
-	}
-	ctx.JSON(http.StatusCreated, gin.H{"message": "update success"})
+  err := ctx.ShouldBindWith(&aircraft, binding.JSON)
+  if err != nil {
+    ctx.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+    return
+  }
+  repositoryError := ac.repository.UpdateAircraft(&aircraft, aircraftId, airlineId)
+  if repositoryError != nil {
+    ctx.JSON(http.StatusBadRequest, gin.H{"Error": repositoryError.Error()})
+    return
+  }
+  ctx.JSON(http.StatusCreated, gin.H{"message": "update success"})
 }
 
 // @Summary Delete aircraft by Id
@@ -146,11 +146,12 @@ func (ac *AircraftController) HandleUpdateAircraft(ctx *gin.Context) {
 // @Success 200  "ok"
 // @Failure 400 "Aircraft not found"
 func (ac *AircraftController) HandleDeleteAircraft(ctx *gin.Context) {
-	aircraftId := ctx.Param("id")
-	err := ac.repository.DeleteAircraft(aircraftId)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"Error": "Incorrect aircraft id: " + aircraftId})
-		return
-	}
-	ctx.JSON(http.StatusOK, "Deleted the aircraft successfully")
+  aircraftId := ctx.Param("id")
+
+  err := ac.repository.DeleteAircraft(aircraftId)
+  if err != nil {
+    ctx.JSON(http.StatusBadRequest, gin.H{"Error": "Incorrect aircraft id: " + aircraftId})
+    return
+  }
+  ctx.JSON(http.StatusOK, "Deleted the aircraft successfully")
 }
