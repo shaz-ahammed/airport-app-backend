063424b4a781186d933bca05875ca9d44420a2f2
Shaz Ahammed <153790994+shaz-ahammed@users.noreply.github.com>
2024-05-06 11:34:37 +0530

    migration file fix (#51)
    migration file fix

--- database/db_migration/V009__PopulateSlotsTable.sql
+++ database/db_migration/V009__PopulateSlotsTable.sql
@@ -1,15 +1,15 @@
-INSERT INTO slots (start_time, end_time, is_available, aircraft_id, gate_id)
+INSERT INTO slots (start_time, end_time, status, aircraft_id, gate_id)
 SELECT
     NOW() - (random() * INTERVAL '30 days') AS start_time,
     NOW() + ((random() * 2 + 2) * INTERVAL '1 hour') AS end_time,
-    false AS is_available,
+    CASE WHEN random() < 0.5 THEN 'Reserved' ELSE 'Booked' END AS status,
     (SELECT id FROM airlines ORDER BY random() LIMIT 1) AS aircraft_id,
     (SELECT id FROM gates ORDER BY random() LIMIT 1) AS gate_id
 FROM generate_series(1, 16);
 
-INSERT INTO slots (start_time, end_time, is_available)
+INSERT INTO slots (start_time, end_time, status)
 SELECT
     NOW() - (random() * INTERVAL '30 days') AS start_time,
     NOW() + ((random() * 2 + 2) * INTERVAL '1 hour') AS end_time,
-    true AS is_available
+    'Available' AS is_available
 FROM generate_series(1, 12);
