65396978eb2d9292b70adb1f23cd1c0201fd3e05
jaicharan <jaicharan.santhanam@thoughtworks.com>
2024-05-23 09:58:32 +0530

    Edited Migration file V_008 for On delete cascade

--- database/db_migration/V008__CreateSlotsTable.sql
+++ database/db_migration/V008__CreateSlotsTable.sql
@@ -3,6 +3,6 @@ CREATE TABLE slots (
     start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     end_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     status VARCHAR(255) DEFAULT 'Available' NOT NULL,
-    aircraft_id UUID REFERENCES airlines (id),
+    aircraft_id UUID  REFERENCES airlines (id),
     gate_id UUID REFERENCES gates (id)
 );
